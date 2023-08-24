52f6a35bae9a84fd9246b79513c7a6bcd5849d5a
rita7811 <92129512+rita7811@users.noreply.github.com>
2023-08-24 19:31:36 -0400

    simple calculator initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Hello, there!");
+
+Console.WriteLine("Please input the first number:");
+var input1 = Console.ReadLine();
+int num1 = int.Parse(input1);
+
+Console.WriteLine("Please input the second number:");
+var input2 = Console.ReadLine();
+int num2 = int.Parse(input2);
+
+Console.WriteLine("What do you want to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+var choice = Console.ReadLine();
+
+if (choice == "A" || choice == "a") {
+    Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+}
+else if (choice == "S" || choice == "s") {
+    Console.WriteLine($"{num1} - {num2} = {num1-num2}");
+}
+else if (choice == "M" || choice == "m") {
+    Console.WriteLine($"{num1} * {num2} = {num1*num2}");
+}
+else {
+    Console.WriteLine("Invalid option!");
+
+}
+
+Console.WriteLine("Press any key to close. Thanks!");
+Console.ReadKey();
+
