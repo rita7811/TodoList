7b25f00ad657b3f642445a59ab69b9c1f61b0677
rita7811 <92129512+rita7811@users.noreply.github.com>
2023-08-24 19:53:58 -0400

    simple calculator code refactoring

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -14,20 +14,30 @@ Console.WriteLine("[S]ubtract");
 Console.WriteLine("[M]ultiply");
 var choice = Console.ReadLine();
 
-if (choice == "A" || choice == "a") {
-    Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+if (EqualsCaseInsensitive(choice, "A")) {
+    var sum = num1 + num2;
+    PrintResult(num1, num2, sum, "+");
 }
-else if (choice == "S" || choice == "s") {
-    Console.WriteLine($"{num1} - {num2} = {num1-num2}");
+else if (EqualsCaseInsensitive(choice, "S")) {
+    var difference = num1 - num2;
+    PrintResult(num1, num2, difference, "-");
 }
-else if (choice == "M" || choice == "m") {
-    Console.WriteLine($"{num1} * {num2} = {num1*num2}");
+else if (EqualsCaseInsensitive(choice, "M")) {
+    var multiplied = num1 * num2;
+    PrintResult(num1, num2, multiplied, "*");
 }
 else {
     Console.WriteLine("Invalid option!");
 
 }
 
+bool EqualsCaseInsensitive (string left, string right) {
+    return left.ToUpper() == right.ToUpper();
+}
+
+void PrintResult(int number1, int number2, int result, string @operator){
+    Console.WriteLine($"{number1} {@operator} {number2} = {result}");
+}
+
 Console.WriteLine("Press any key to close. Thanks!");
 Console.ReadKey();
-
